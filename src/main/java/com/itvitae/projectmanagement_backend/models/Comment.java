c8af134a78bd0a4b5e8ba863aacdac79bc95b8ee
Chun <beerdsenc@gmail.com>
2025-11-04 17:32:20 +0100

    Added the @Entity annotation

--- src/main/java/com/itvitae/projectmanagement_backend/models/Comment.java
+++ src/main/java/com/itvitae/projectmanagement_backend/models/Comment.java
@@ -1,9 +1,11 @@
 package com.itvitae.projectmanagement_backend.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
