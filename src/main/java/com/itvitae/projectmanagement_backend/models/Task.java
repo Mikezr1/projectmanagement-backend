a937443cc2aa9b0ff4067ae1f81f1ff0148cf774
Chun <beerdsenc@gmail.com>
2025-11-04 17:27:05 +0100

    Added Task variables constructors and getters and setters

---
+++ src/main/java/com/itvitae/projectmanagement_backend/models/Task.java
@@ -0,0 +1,62 @@
+package com.itvitae.projectmanagement_backend.models;
+
+import com.itvitae.projectmanagement_backend.enums.Status;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private String description;
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public Task() {}
+    public Task(String title, String description, List<Comment> comments, Status status) {
+        this.title = title;
+        this.description = description;
+        this.comments = comments;
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
