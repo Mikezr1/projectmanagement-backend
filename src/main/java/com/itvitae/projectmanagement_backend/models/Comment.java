22e46d038692d9341b61eb007387e8d0dbb73979
Chun <beerdsenc@gmail.com>
2025-11-04 17:25:42 +0100

    Added Comment variables constructors and getters and setters

---
+++ src/main/java/com/itvitae/projectmanagement_backend/models/Comment.java
@@ -0,0 +1,37 @@
+package com.itvitae.projectmanagement_backend.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private User poster;
+    private String description;
+
+    public Comment() {}
+    public Comment(User poster, String description) {
+        this.poster = poster;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public User getPoster() {
+        return poster;
+    }
+    public void setPoster(User poster) {
+        this.poster = poster;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
