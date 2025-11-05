7b037bd47c3792735c1f096abf5da388311db955
Chun <beerdsenc@gmail.com>
2025-11-05 14:58:44 +0100

    Renamed TaskResponseDTO to TaskSummaryDTO

--- src/main/java/com/itvitae/projectmanagement_backend/dto/comment/CommentSummaryDTO.java
+++ src/main/java/com/itvitae/projectmanagement_backend/dto/comment/CommentSummaryDTO.java
@@ -2,7 +2,7 @@ package com.itvitae.projectmanagement_backend.dto.comment;
 
 import com.itvitae.projectmanagement_backend.models.User;
 
-public record CommentResponseDTO(
+public record CommentSummaryDTO(
     long id,
     User poster,
     String description
--- src/main/java/com/itvitae/projectmanagement_backend/dto/task/TaskSummaryDTO.java
+++ src/main/java/com/itvitae/projectmanagement_backend/dto/task/TaskSummaryDTO.java
@@ -1,6 +1,6 @@
 package com.itvitae.projectmanagement_backend.dto.task;
 
-import com.itvitae.projectmanagement_backend.dto.comment.CommentResponseDTO;
+import com.itvitae.projectmanagement_backend.dto.comment.CommentSummaryDTO;
 import com.itvitae.projectmanagement_backend.enums.Status;
 
 import java.util.List;
@@ -9,6 +9,6 @@ public record TaskSummaryDTO(
     long id,
     String title,
     String description,
-    List<CommentResponseDTO> comments,
+    List<CommentSummaryDTO> comments,
     Status status
 ) {}
