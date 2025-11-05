3c3aeba664e721c72b20f51e5ace8fef436def28
Chun <beerdsenc@gmail.com>
2025-11-05 14:57:43 +0100

    Added TaskService methods

--- src/main/java/com/itvitae/projectmanagement_backend/services/TaskService.java
+++ src/main/java/com/itvitae/projectmanagement_backend/services/TaskService.java
@@ -1,22 +1,93 @@
 package com.itvitae.projectmanagement_backend.services;
 
+import com.itvitae.projectmanagement_backend.dto.mappers.TaskMapper;
 import com.itvitae.projectmanagement_backend.dto.task.*;
+import com.itvitae.projectmanagement_backend.exceptions.TaskNotFoundException;
+import com.itvitae.projectmanagement_backend.exceptions.UserNotFoundException;
+import com.itvitae.projectmanagement_backend.models.Task;
+import com.itvitae.projectmanagement_backend.models.User;
+import com.itvitae.projectmanagement_backend.repositories.CommentRepository;
+import com.itvitae.projectmanagement_backend.repositories.TaskRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
-    public TaskResponseDTO createTask(TaskCreateDTO taskCreateDTO) {
+    private final TaskRepository taskRepository;
+    private final CommentRepository commentRepository;
+    private final TaskMapper taskMapper;
+
+    public TaskService(TaskRepository taskRepository, CommentRepository commentRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.commentRepository = commentRepository;
+        this.taskMapper = taskMapper;
     }
 
-    public TaskResponseDTO updateStatus(TaskUpdateStatusDTO dto) {
+    @Transactional
+    public TaskSummaryDTO createTask(TaskCreateDTO dto) {
+        User user = userRepository.findById(dto.userId())
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        Task task = taskMapper.toEntity(dto, user);
+        taskRepository.save(task);
+        return taskMapper.toDTO(task);
     }
 
-    public TaskResponseDTO updatetitle(TaskUpdateTitleDTO dto) {
+    @Transactional
+    public List<TaskSummaryDTO> getAllTasks() {
+        List<Task> tasks = taskRepository.findAll();
+
+        return tasks.stream()
+                .map(taskMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
-    public TaskResponseDTO updateDescription(TaskUpdateDescriptionDTO dto) {
+    @Transactional
+    public TaskSummaryDTO updateStatus(Long id, TaskUpdateStatusDTO dto) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found"));
+
+        taskMapper.TaskUpdateStatusDTO(task, dto);
+        taskRepository.save(task);
+        return taskMapper.toDTO(task);
     }
 
+    @Transactional
+    public TaskSummaryDTO updatetitle(Long id, TaskUpdateTitleDTO dto) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found"));
+
+        taskMapper.TaskUpdateTitleDTO(task, dto);
+        taskRepository.save(task);
+        return taskMapper.toDTO(task);
+    }
+
+    @Transactional
+    public TaskSummaryDTO updateDescription(Long id, TaskUpdateDescriptionDTO dto) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found"));
+
+        taskMapper.TaskUpdateDescriptionDTO(task, dto);
+        taskRepository.save(task);
+        return taskMapper.toDTO(task);
+    }
+
+    @Transactional
     public void deleteTask(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found"));
+
+        taskRepository.delete(task);
+    }
+
+    @Transactional
+    public TaskSummaryDTO getTaskById(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found"));
+
+        return taskMapper.toDTO(task);
     }
 }
