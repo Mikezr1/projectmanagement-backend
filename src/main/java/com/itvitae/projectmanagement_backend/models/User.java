9b846efa4fc0aed832ac638be17840df1ed0d963
ThomazSDA <thomas_wennink@hotmail.com>
2025-11-06 11:33:33 +0100

    Added UserEntity variables

--- src/main/java/com/itvitae/projectmanagement_backend/models/User.java
+++ src/main/java/com/itvitae/projectmanagement_backend/models/User.java
@@ -15,62 +15,35 @@ public class User {
 
     private String firstName;
     private String lastName;
-    private String password;
+    private String password; //TODO Stefan: create Hash instead of String
     private String email;
     private String companyName;
 
-    public User() {
-    }
+    public User() { }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
-    public UserType getType() {
-        return type;
-    }
+    public UserType getType() { return type; }
 
-    public void setType(UserType type) {
-        this.type = type;
-    }
+    public void setType(UserType type) { this.type = type; }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getFirstName() { return firstName; }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public String getLastName() {
-        return lastName;
-    }
+    public String getLastName() { return lastName; }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
-    public String getPassword() {
-        return password;
-    }
+    public String getPassword() { return password; }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public void setPassword(String password) { this.password = password; }
 
-    public String getEmail() {
-        return email;
-    }
+    public String getEmail() { return email; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public void setEmail(String email) { this.email = email; }
 
-    public String getCompanyName() {
-        return companyName;
-    }
+    public String getCompanyName() {  return companyName; }
 
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
+    public void setCompanyName(String companyName) { this.companyName = companyName; }
 }
