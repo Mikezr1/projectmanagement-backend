246bb1b880e430c0f768b03522c75b957a2bf73a
Chun <beerdsenc@gmail.com>
2025-11-06 15:29:03 +0100

    Added User to task

--- src/main/java/com/itvitae/projectmanagement_backend/dto/task/TaskUpdateStatusDTO.java
+++ src/main/java/com/itvitae/projectmanagement_backend/dto/task/TaskUpdateStatusDTO.java
@@ -1,9 +1,11 @@
 package com.itvitae.projectmanagement_backend.dto.task;
 
 import com.itvitae.projectmanagement_backend.enums.Status;
+import com.itvitae.projectmanagement_backend.models.User;
 
 public record TaskUpdateStatusDTO(
         Long id,
-        Status status
+        Status status,
+        User user
 ) {
 }
