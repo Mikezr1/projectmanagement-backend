aba081fe94b53c49738402b0007ba535fbf8d8e2
Chun <beerdsenc@gmail.com>
2025-11-06 11:48:10 +0100

    Added Role enum and changed Type enum to Permission

---
+++ src/main/java/com/itvitae/projectmanagement_backend/enums/Permission.java
@@ -0,0 +1,12 @@
+package com.itvitae.projectmanagement_backend.enums;
+
+public enum Permission {
+    CREATE_TASK,
+    DELETE_TASK,
+    UPDATE_TASK,
+    READ_TASK,
+    CREATE_COMMENT,
+    DELETE_COMMENT,
+    UPDATE_COMMENT,
+    READ_COMMENT
+}
