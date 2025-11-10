600bf376215a237cfe83c96168a332f55dccd76f
Chun <beerdsenc@gmail.com>
2025-11-10 21:01:54 +0100

    updated all services

--- src/main/java/com/itvitae/projectmanagement_backend/services/TaskService.java
+++ src/main/java/com/itvitae/projectmanagement_backend/services/TaskService.java
@@ -2,14 +2,13 @@ package com.itvitae.projectmanagement_backend.services;
 
 import com.itvitae.projectmanagement_backend.dto.mappers.TaskMapper;
 import com.itvitae.projectmanagement_backend.dto.task.*;
+import com.itvitae.projectmanagement_backend.exceptions.ProjectNotFoundException;
 import com.itvitae.projectmanagement_backend.exceptions.TaskNotFoundException;
 import com.itvitae.projectmanagement_backend.exceptions.UserNotFoundException;
+import com.itvitae.projectmanagement_backend.models.Project;
 import com.itvitae.projectmanagement_backend.models.Task;
 import com.itvitae.projectmanagement_backend.models.User;
-import com.itvitae.projectmanagement_backend.repositories.CommentRepository;
-import com.itvitae.projectmanagement_backend.repositories.NotificationRepository;
-import com.itvitae.projectmanagement_backend.repositories.TaskRepository;
-import com.itvitae.projectmanagement_backend.repositories.UserRepository;
+import com.itvitae.projectmanagement_backend.repositories.*;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -19,16 +18,16 @@ import java.util.stream.Collectors;
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
-    private final CommentRepository commentRepository;
     private final UserRepository userRepository;
     private final NotificationRepository notificationRepository;
+    private final ProjectRepository projectRepository;
     private final TaskMapper taskMapper;
 
-    public TaskService(TaskRepository taskRepository, CommentRepository commentRepository, UserRepository userRepository, NotificationRepository notificationRepository, TaskMapper taskMapper) {
+    public TaskService(TaskRepository taskRepository, UserRepository userRepository, NotificationRepository notificationRepository, ProjectRepository projectRepository, TaskMapper taskMapper) {
         this.taskRepository = taskRepository;
-        this.commentRepository = commentRepository;
         this.userRepository = userRepository;
         this.notificationRepository = notificationRepository;
+        this.projectRepository = projectRepository;
         this.taskMapper = taskMapper;
     }
 
@@ -37,25 +36,27 @@ public class TaskService {
         User user = userRepository.findById(dto.userId())
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
 
-        Task task = taskMapper.toEntity(dto, user);
+        Project project = projectRepository.findById(dto.projectId())
+                .orElseThrow(() -> new ProjectNotFoundException("Project not found"));
+
+        Task task = taskMapper.toEntity(dto, user, project);
         taskRepository.save(task);
-
-//        Notification notification = new Notification(
-//                "Task created: " + task.getTitle(),
-//                user
-//        );
-
-//        notificationRepository.save(notification);
         return taskMapper.toDTO(task);
     }
 
     @Transactional
     public List<TaskSummaryDTO> getAllTasks() {
-        List<Task> tasks = taskRepository.findAll();
-
-        return tasks.stream()
+        return taskRepository.findAll()
+                .stream()
                 .map(taskMapper::toDTO)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public TaskSummaryDTO getTaskById(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found"));
+        return taskMapper.toDTO(task);
     }
 
     @Transactional
@@ -72,20 +73,8 @@ public class TaskService {
     public void deleteTask(Long id) {
         Task task = taskRepository.findById(id)
                 .orElseThrow(() -> new TaskNotFoundException("Task not found"));
-
-//        Notification notification = new Notification(
-//                "Task deleted: " + task.getTitle()
-//        );
-//        notificationRepository.save(notification);
-
         taskRepository.delete(task);
     }
 
-    @Transactional
-    public TaskSummaryDTO getTaskById(Long id) {
-        Task task = taskRepository.findById(id)
-                .orElseThrow(() -> new TaskNotFoundException("Task not found"));
 
-        return taskMapper.toDTO(task);
-    }
 }
