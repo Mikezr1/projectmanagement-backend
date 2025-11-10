2621e68877dbb8c82e63da54c147a2d6196eea2c
Chun <beerdsenc@gmail.com>
2025-11-10 15:46:29 +0100

    Updated user dtos

---
+++ src/main/java/com/itvitae/projectmanagement_backend/dto/mappers/UserMapper.java
@@ -0,0 +1,42 @@
+package com.itvitae.projectmanagement_backend.dto.mappers;
+
+import com.itvitae.projectmanagement_backend.dto.user.UserCreateDTO;
+import com.itvitae.projectmanagement_backend.dto.user.UserSummaryDTO;
+import com.itvitae.projectmanagement_backend.dto.user.UserUpdateDTO;
+import com.itvitae.projectmanagement_backend.models.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public User toEntity(UserCreateDTO dto) {
+        User user = new User();
+        user.setFirstName(dto.firstName());
+        user.setLastName(dto.lastName());
+        user.setEmail(dto.email());
+        user.setRole(dto.role());
+        user.setCompanyName(dto.companyName());
+        return user;
+    }
+
+    public void updateFromDTO(User user, UserUpdateDTO dto) {
+        if (dto.firstName() != null) {
+            user.setFirstName(dto.firstName());
+        }
+        if (dto.lastName() != null) {
+            user.setLastName(dto.lastName());
+        }
+        if (dto.role() != null) {
+            user.setRole(dto.role());
+        }
+        //More updates here
+    }
+
+    public UserSummaryDTO toDTO(User user) {
+        return new UserSummaryDTO(
+                user.getId(),
+                user.getFirstName(),
+                user.getEmail(),
+                user.getRole()
+        );
+    }
+}
