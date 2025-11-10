3ad6c0e18d9391aaecfcb46412359f1ba4efef92
Chun <beerdsenc@gmail.com>
2025-11-10 17:48:40 +0100

    Updated all controllers

--- src/main/java/com/itvitae/projectmanagement_backend/controllers/ProjectController.java
+++ src/main/java/com/itvitae/projectmanagement_backend/controllers/ProjectController.java
@@ -2,18 +2,21 @@ package com.itvitae.projectmanagement_backend.controllers;
 
 import com.itvitae.projectmanagement_backend.dto.project.ProjectCreateDTO;
 import com.itvitae.projectmanagement_backend.dto.project.ProjectDTO;
+import com.itvitae.projectmanagement_backend.dto.project.ProjectSummaryDTO;
+import com.itvitae.projectmanagement_backend.dto.project.ProjectUpdateDTO;
 import com.itvitae.projectmanagement_backend.services.ProjectService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
 @RestController
-@CrossOrigin("*")
-@RequestMapping("/project")
+@CrossOrigin(origins = "*")
+@RequestMapping("/projects")
 public class ProjectController {
     final private ProjectService projectService;
 
@@ -23,26 +26,30 @@ public class ProjectController {
     }
 
     @PostMapping
-    public ResponseEntity<ProjectDTO> createProject(@Valid @RequestBody ProjectCreateDTO createDTO) {
-        ProjectDTO created = projectService.createProject(createDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    @PreAuthorize("hasAuthority('ADD_USER')")
+    public ResponseEntity<ProjectDTO> createProject(@Valid @RequestBody ProjectCreateDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.createProject(dto));
     }
 
     @GetMapping
-    public ResponseEntity<List<ProjectDTO>> listAllProjects(){
+    public ResponseEntity<List<ProjectDTO>> getAllProjects(){
         List<ProjectDTO> projects = projectService.findAll();
         return ResponseEntity.ok(projects);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProjectDTO> getProject(@PathVariable Long id){
-        ProjectDTO project = projectService.findById(id);
-        return ResponseEntity.ok(project);
+    public ResponseEntity<ProjectDTO> getProject(@PathVariable Long id) {
+        return ResponseEntity.ok(projectService.findById(id));
     }
-//
-//    @PutMapping
-//    //TODO stefan wijzigingen in project (project UPdate DTO)
-//
-//    @DeleteMapping
-//    //TODO wijziging delete geheel ipv dto (response dto void?)
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProjectSummaryDTO> updateProject(@PathVariable Long id, @RequestBody ProjectUpdateDTO dto) {
+        return ResponseEntity.ok(projectService.updateProject(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProject(@PathVariable Long id) {
+        projectService.deleteProject(id);
+        return ResponseEntity.noContent().build();
+    }
 }
