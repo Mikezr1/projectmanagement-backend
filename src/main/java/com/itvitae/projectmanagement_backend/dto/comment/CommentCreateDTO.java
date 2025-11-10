a11e72854a1f473c4f4ec1c0917dc6155ec55aa2
Chun <beerdsenc@gmail.com>
2025-11-10 15:47:09 +0100

    Updated comment dtos

--- src/main/java/com/itvitae/projectmanagement_backend/dto/comment/CommentCreateDTO.java
+++ src/main/java/com/itvitae/projectmanagement_backend/dto/comment/CommentCreateDTO.java
@@ -2,6 +2,6 @@ package com.itvitae.projectmanagement_backend.dto.comment;
 
 public record CommentCreateDTO(
     String description,
-    Long userId,
-    Long taskId
+    Long taskId,
+    Long userId
 ) {}
