3266ab2786dd0ab936d1fbcafd84babbc0ecb558
Chun <beerdsenc@gmail.com>
2025-11-10 15:45:12 +0100

    Deleted left over update dtos

--- src/main/java/com/itvitae/projectmanagement_backend/dto/task/TaskUpdateDescriptionDTO.java
+++
@@ -1,9 +0,0 @@
-package com.itvitae.projectmanagement_backend.dto.task;
-
-import com.itvitae.projectmanagement_backend.models.User;
-
-public record TaskUpdateDescriptionDTO(
-        Long id,
-        String description,
-        User user
-) {}
--- src/main/java/com/itvitae/projectmanagement_backend/dto/task/TaskUpdateTitleDTO.java
+++
@@ -1,9 +0,0 @@
-package com.itvitae.projectmanagement_backend.dto.task;
-
-import com.itvitae.projectmanagement_backend.models.User;
-
-public record TaskUpdateTitleDTO(
-   Long id,
-   String title,
-   User user
-) {}
